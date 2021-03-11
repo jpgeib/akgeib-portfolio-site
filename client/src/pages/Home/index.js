fc68d3231103b7aaa0c89c7ed2d77c7de6951378
James Geib <jpgeib2@gmail.com>
2021-03-11 14:21:42 -0500

    minor changes

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,1 +1,16 @@
 import React, { Component } from "react";
+import { Grid, Header } from "semantic-ui-react";
+
+import "./style.css";
+
+class Home extends Component {
+    render() {
+        return(
+            <>
+
+            </>
+        );
+    }
+}
+
+export default Home;
